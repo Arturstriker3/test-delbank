9c54312217b608fb956c2257c650baeb7cb6229f
Arturstriker3 <artur.danielgama@gmail.com>
2024-04-09 13:29:06 -0300

    "Feat": Implementaçaõ do Front com autenticação

---
+++ front/src/views/index.js
@@ -0,0 +1,4 @@
+export { default as HomeView } from './HomeView.vue';
+export { default as LoginView } from './LoginView.vue';
+export { default as CreatePixView } from './CreatePixView.vue';
+export { default as PayPixView } from './PayPixView.vue';
